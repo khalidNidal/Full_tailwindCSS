6c1ba07a0f57fc8895ebdb76bf65d365afefef40
khalid nidal <kahlid_s@hottmail.com>
2023-09-03 11:26:06 +0300

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+const mobilebtn = document.querySelector("#mobile-btn");
+const mobilemenu = document.querySelector("#mobile-menu");
+
+mobilebtn.addEventListener('click', ()=>{
+    mobilemenu.classList.toggle('hidden');
+});
+
+
